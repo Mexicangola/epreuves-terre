520445b85d3d5d67edc7234ed0c48bd961d4776c
Ana <gutierrez.moyano.ana@gmail.com>
2025-01-23 17:55:47 +0100

    ajout de l'epreuve de la terre00

---
+++ terre01.java
@@ -0,0 +1,10 @@
+public class terre01
+{
+    public static void main(String[] args)
+    {
+        for(char c = 'a' ; c <= 'z' ; c++){
+            System.out.print(c);
+        }
+        System.out.println();
+    }
+}
