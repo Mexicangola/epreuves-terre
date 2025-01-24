42e2a73f12a3d106e875ea62a1ea14f8c00a327d
Ana <gutierrez.moyano.ana@gmail.com>
2025-01-24 09:07:23 +0100

    ajout de l'epreuve de la terre01

---
+++ terre01.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class terre01
+{
+    public static void main(String[] args)
+    {
+        String className = terre01.class.getSimpleName();// Récupérer le nom de la classe
+
+        String fileName = className + ".java"; // Ajouter l'extension ".java"
+
+        System.out.println("Le nom du fichier est : " + fileName);// Afficher le nom du fichier
+    }
+}
